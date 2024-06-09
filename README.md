392050740c602036c9d5e590180b854e22d42d4e
Dmitry Naumov <d.naumov@adguard.com>
2024-06-09 03:14:30 +0300

    Support for several proxmox nodes; cluster/storage metrics; cluster node cpu/memory metrics; general fixes

--- README.md
+++ README.md
@@ -1,21 +1,35 @@
 # The simplest Prometheus exporter for Proxmox
 
-### Available metrics:
-* proxmox_vm_status
-* proxmox_vm_diskwrite
-* proxmox_vm_vmid
-* proxmox_vm_pid
-* proxmox_vm_cpus
-* proxmox_vm_cpu
-* proxmox_vm_netout
-* proxmox_vm_maxdisk
-* proxmox_vm_name
-* proxmox_vm_diskread
-* proxmox_vm_mem
-* proxmox_vm_netin
-* proxmox_vm_maxmem
-* proxmox_vm_uptime
-* proxmox_vm_disk
+### Available metrics
+## VM Metrics
+  * `proxmox_vm_status`
+  * `proxmox_vm_diskwrite`
+  * `proxmox_vm_vmid`
+  * `proxmox_vm_pid`
+  * `proxmox_vm_cpus`
+  * `proxmox_vm_cpu`
+  * `proxmox_vm_netout`
+  * `proxmox_vm_maxdisk`
+  * `proxmox_vm_name`
+  * `proxmox_vm_diskread`
+  * `proxmox_vm_mem`
+  * `proxmox_vm_netin`
+  * `proxmox_vm_maxmem`
+  * `proxmox_vm_uptime`
+  * `proxmox_vm_disk`
+
+## Node Metrics
+  * `proxmox_node_cpu_usage`
+  * `proxmox_node_memory_total_bytes`
+  * `proxmox_node_memory_free_bytes`
+
+## Storage Metrics
+  * `proxmox_storage_info` - Information about Proxmox storages (labels: `storage`, `type`, `shared`)
+  * `proxmox_storage_type_count` - Count of storages by type (labels: `type`)
+
+## Cluster Metrics
+  * `proxmox_cluster_*` - Various cluster metrics based on the Proxmox API response
+
 
 ### Installation:
 `go build`
@@ -30,7 +44,3 @@ Available flags:
 * `-listen.address` - address to bind (default: `:9914`)
 * `-config.file` - path to a configuration file (default: `/etc/proxmox_exporter.yaml`)
 * `-log.level` - logging level (default: `none`)
-
-### TODO (contact me if you need it):
-* Support multiple "datacenters"
-* Support CT
--- app.go
+++ app.go
@@ -15,7 +15,7 @@ import (
 )
 
 var (
-	configFile    = flag.String("config.file", "/etc/proxmox_exporter.yaml", "Path to a configuration file")
+	configFile    = flag.String("config.file", "./config.yaml", "Path to a configuration file")
 	listenAddress = flag.String("listen.address", "127.0.0.1:9914", "Address to bind")
 	logLevel      = flag.String("log.level", "none", "Logging level")
 )
@@ -26,7 +26,9 @@ func isNumeric(s interface{}) bool {
 }
 
 type proxmoxCollector struct {
-	config map[string]string
+	config   map[string]string
+	baseURL  string
+	token    string
 }
 
 func (collector *proxmoxCollector) Describe(ch chan<- *prometheus.Desc) {
@@ -34,45 +36,155 @@ func (collector *proxmoxCollector) Describe(ch chan<- *prometheus.Desc) {
 }
 
 func (collector *proxmoxCollector) Collect(ch chan<- prometheus.Metric) {
-	token_string := "PVEAPIToken=" + collector.config["proxmox_username"] + "=" + collector.config["proxmox_token"]
-	proxmox_api_url_nodes := "https://" + collector.config["proxmox_ip"] + ":" + collector.config["proxmox_port"] + "/api2/json/nodes/" + collector.config["proxmox_node"] + "/qemu"
-
 	client := &http.Client{}
-	req, _ := http.NewRequest("GET", proxmox_api_url_nodes, nil)
-	req.Header.Add("Authorization", token_string)
+
+	// Collect cluster metrics
+	clusterStatus := collector.fetchJSON(client, "/cluster/status")
+	if data, ok := clusterStatus["data"].([]interface{}); ok {
+		for _, item := range data {
+			if itemMap, ok := item.(map[string]interface{}); ok {
+				for key, value := range itemMap {
+					if isNumeric(value) {
+						metricName := fmt.Sprintf("proxmox_cluster_%s", key)
+						metricValue, _ := value.(json.Number).Float64()
+						ch <- prometheus.MustNewConstMetric(
+							prometheus.NewDesc(metricName, "Proxmox cluster metric", nil, nil),
+							prometheus.GaugeValue,
+							metricValue,
+						)
+					}
+				}
+			}
+		}
+	}
+
+	// Collect storage metrics
+	storageResponse := collector.fetchJSON(client, "/storage")
+	if data, ok := storageResponse["data"].([]interface{}); ok {
+		storageTypeCount := make(map[string]int)
+		for _, item := range data {
+			if itemMap, ok := item.(map[string]interface{}); ok {
+				storageName := fmt.Sprintf("%v", itemMap["storage"])
+				storageType := fmt.Sprintf("%v", itemMap["type"])
+				isShared := "0"
+				if shared, ok := itemMap["shared"].(json.Number); ok && shared.String() == "1" {
+					isShared = "1"
+				}
+
+				storageTypeCount[storageType]++
+
+				ch <- prometheus.MustNewConstMetric(
+					prometheus.NewDesc("proxmox_storage_info", "Information about Proxmox storages", []string{"storage", "type", "shared"}, nil),
+					prometheus.GaugeValue,
+					1,
+					storageName,
+					storageType,
+					isShared,
+				)
+			}
+		}
+
+		for storageType, count := range storageTypeCount {
+			ch <- prometheus.MustNewConstMetric(
+				prometheus.NewDesc("proxmox_storage_type_count", "Count of storages by type", []string{"type"}, nil),
+				prometheus.GaugeValue,
+				float64(count),
+				storageType,
+			)
+		}
+	}
+
+	// Collect node and VM metrics
+	nodesResponse := collector.fetchJSON(client, "/nodes")
+	if nodes, ok := nodesResponse["data"].([]interface{}); ok {
+		for _, node := range nodes {
+			if nodeMap, ok := node.(map[string]interface{}); ok {
+				nodeName := fmt.Sprintf("%v", nodeMap["node"])
+
+				// Collect node metrics
+				nodeStatus := collector.fetchJSON(client, "/nodes/"+nodeName+"/status")
+				if data, ok := nodeStatus["data"].(map[string]interface{}); ok {
+					if cpu, ok := data["cpu"].(json.Number); ok {
+						cpuValue, _ := cpu.Float64()
+						ch <- prometheus.MustNewConstMetric(
+							prometheus.NewDesc("proxmox_node_cpu_usage", "Proxmox node CPU usage", []string{"node"}, nil),
+							prometheus.GaugeValue,
+							cpuValue,
+							nodeName,
+						)
+					}
+					if memory, ok := data["memory"].(map[string]interface{}); ok {
+						memTotal, _ := memory["total"].(json.Number).Float64()
+						memFree, _ := memory["free"].(json.Number).Float64()
+						ch <- prometheus.MustNewConstMetric(
+							prometheus.NewDesc("proxmox_node_memory_total_bytes", "Proxmox node total memory in bytes", []string{"node"}, nil),
+							prometheus.GaugeValue,
+							memTotal,
+							nodeName,
+						)
+						ch <- prometheus.MustNewConstMetric(
+							prometheus.NewDesc("proxmox_node_memory_free_bytes", "Proxmox node free memory in bytes", []string{"node"}, nil),
+							prometheus.GaugeValue,
+							memFree,
+							nodeName,
+						)
+					}
+				}
+
+				// Collect VM metrics
+				vmsResponse := collector.fetchJSON(client, "/nodes/"+nodeName+"/qemu")
+				if vms, ok := vmsResponse["data"].([]interface{}); ok {
+					for _, vm := range vms {
+						if vmMap, ok := vm.(map[string]interface{}); ok {
+							vmid := fmt.Sprintf("%v", vmMap["vmid"])
+							vmname := fmt.Sprintf("%v", vmMap["name"])
+
+							// Fetch VM status
+							vmStatus := collector.fetchJSON(client, "/nodes/"+nodeName+"/qemu/"+vmid+"/status/current")
+							if data, ok := vmStatus["data"].(map[string]interface{}); ok {
+								for key, value := range data {
+									if isNumeric(value) {
+										metricName := fmt.Sprintf("proxmox_vm_%s", key)
+										metricValue, _ := value.(json.Number).Float64()
+										ch <- prometheus.MustNewConstMetric(
+											prometheus.NewDesc(metricName, "Proxmox VM metric", []string{"vmid", "vmname", "proxmox_node"}, nil),
+											prometheus.GaugeValue,
+											metricValue,
+											vmid,
+											vmname,
+											nodeName,
+										)
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+func (collector *proxmoxCollector) fetchJSON(client *http.Client, path string) map[string]interface{} {
+	req, _ := http.NewRequest("GET", collector.baseURL+path, nil)
+	req.Header.Add("Authorization", collector.token)
 
 	res, err := client.Do(req)
 	if err != nil {
 		fmt.Println(err)
-		return
+		return nil
 	}
+	defer res.Body.Close()
 
-	m := map[string][]map[string]interface{}{}
+	response := map[string]interface{}{}
 	dec := json.NewDecoder(res.Body)
 	dec.UseNumber()
-	err = dec.Decode(&m)
+	err = dec.Decode(&response)
 	if err != nil {
 		fmt.Println(err)
-		return
+		return nil
 	}
-
-    for _, v := range m["data"] {
-        vmid := fmt.Sprintf("%v", v["vmid"])
-        vmname := fmt.Sprintf("%v", v["name"])
-        for key, value := range v {
-            if isNumeric(value) {
-                metricName := fmt.Sprintf("proxmox_vm_%s", key)
-                metricValue, _ := value.(json.Number).Float64()
-                ch <- prometheus.MustNewConstMetric(
-                    prometheus.NewDesc(metricName, "Proxmox metric", []string{"vmid", "vmname"}, nil),
-                    prometheus.GaugeValue,
-                    metricValue,
-                    vmid,
-                    vmname,
-                )
-            }
-        }
-    }
+	return response
 }
 
 func main() {
@@ -86,10 +198,16 @@ func main() {
 		panic(err)
 	}
 
-	y := map[string]string{}
-	err = yaml.Unmarshal(yamlFile, &y)
+	config := map[string]string{}
+	err = yaml.Unmarshal(yamlFile, &config)
+	if err != nil {
+		panic(err)
+	}
 
-	prometheus.MustRegister(&proxmoxCollector{config: y})
+	baseURL := "https://" + config["proxmox_address"] + ":" + config["proxmox_port"] + "/api2/json"
+	token := "PVEAPIToken=" + config["proxmox_username"] + "=" + config["proxmox_token"]
+
+	prometheus.MustRegister(&proxmoxCollector{config: config, baseURL: baseURL, token: token})
 
 	http.Handle("/metrics", promhttp.Handler())
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
